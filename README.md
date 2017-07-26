19e5923b21a24e9e2d3e5d4fe47956b1c8d452dc
Jason <yhorg@hotmail.com>
2017-07-26 14:50:16 +0800

    Update building script and README.md.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Eufa
 A low-level efficient utility functions library written in webassembly.
+
+### Build
+```shell
+# Install dependencies
+npm install
+
+# Build
+npm run build
+
+# Run test
+npm run test
+```
+
+### Dependencies
+* Node
+* NPM
+* Emsdk
--- bin/build.sh
+++ bin/build.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
 
+# Global
+EUFA_SOURCE_FOLDER='compiler/es5'
+EUFA_BABEL_TRANSFORM_LIST='eufa-post-compile.js eufa-entrance.js'
+EUFA_POST_COMPILE_EMBED_LIST='eufa-post-compile.js'
+
+
 # Babel transform
-node node_modules/babel-cli/bin/babel.js  compiler/eufa-post-compile.js -o compiler/es5/eufa-post-compile.js
-node node_modules/babel-cli/bin/babel.js compiler/eufa-post-save-instance.js -o compiler/es5/eufa-post-save-instance.js
-node node_modules/babel-cli/bin/babel.js compiler/eufa-entrance.js -o compiler/es5/eufa-entrance.js
+if [ $(command -v node) ]
+then
+    for file in $EUFA_BABEL_TRANSFORM_LIST
+    do
+        node node_modules/babel-cli/bin/babel.js compiler/$file -o $EUFA_SOURCE_FOLDER/$file
+    done
+else
+    echo "[Eufa] Command 'node' not found, please install 'node' first."
+    exit 1
+fi
 
-# Compiling
-emcc -s WASM=1 -O3 -o dist/eufa.js src/build.cpp --post-js compiler/es5/eufa-post-compile.js --post-js compiler/es5/eufa-post-save-instance.js
 
-# Copying files
+# Babel transform
+if [ $(command -v emcc) ]
+then
+    # Compiling
+    _TSTRING=""
+
+    for file in $EUFA_POST_COMPILE_EMBED_LIST
+    do
+        _TSTRING="${_TSTRING} --post-js $EUFA_SOURCE_FOLDER/$file"
+    done
+
+    emcc -s WASM=1 -O3 -o dist/eufa.js src/build.cpp $_TSTRING
+else
+    echo "[Eufa] Command 'emcc' not found, please install 'emsdk' first."
+    exit 1
+fi
+
+
+# Copying main files
 cp compiler/es5/eufa-entrance.js dist/eufa-entrance.js
 
-echo "[Eufa] Building... complete!"
+
+echo "[Eufa] Building... complete!!!"
+exit 0
